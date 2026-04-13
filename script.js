c148997639e7836e46e4f0f890403d412cef0123
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-04-13 13:56:28 +0500

    added play pause resume restart reverse

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded" ,(e)=>{
     console.log(e , gsap)
-    gsap.to(".box" , {
+   let tweenTp = gsap.to(".box" , {
         x:300,
-        duration:2,
+        duration:16,
         ease:"back" 
     })
+
+
+    document.querySelector("#play").onclick =()=> tweenTp.play()
+    document.querySelector("#pause").onclick =()=> tweenTp.pause()
+    document.querySelector("#resume").onclick =()=> tweenTp.resume()
+    document.querySelector("#restart").onclick =()=> tweenTp.restart()
+    document.querySelector("#reverse").onclick =()=> tweenTp.reverse()
 })
