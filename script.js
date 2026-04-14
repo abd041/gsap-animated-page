bc590d56e349787094eeb60eafd42b3a2967556c
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-04-14 18:23:57 +0500

    gsap.from completed

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded" ,(e)=>{
-    console.log(e , gsap)
-   let tweenTp = gsap.to(".box" , {
-        x:300,
-        duration:3,
-        ease:"back",
-        onComplete:()=>{
-            console.log("animation complted")
-        },
-        repeat:-1 , // to run it for infinite time
+//     console.log(e , gsap)
+//    let tweenTp = gsap.to(".box" , {
+//         x:300,
+//         duration:3,
+//         ease:"back",
+//         onComplete:()=>{
+//             console.log("animation complted")
+//         },
+//         repeat:-1 , // to run it for infinite time
 
-    })
+//     })
+
+
+// Gsap from method to animate the objects from a specific position to current position
+
+
+gsap.from(".box" ,{
+    x:300,
+    opacity:0,
+    duration:2
+
+})
 
 
     document.querySelector("#play").onclick =()=> tweenTp.play()
