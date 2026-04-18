9d01b6c5eafbd2d536785698fb87559b87f8152e
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-04-18 18:37:16 +0500

    slides moving away animation completed

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-document.addEventListener("DOMContentLoaded" ,(e)=>{
-//     console.log(e , gsap)
-//    let tweenTp = gsap.to(".box" , {
-//         x:300,
-//         duration:3,
-//         ease:"back",
-//         onComplete:()=>{
-//             console.log("animation complted")
-//         },
-//         repeat:-1 , // to run it for infinite time
-
-//     })
-
-
-// Gsap from method to animate the objects from a specific position to current position
-
-
-// gsap.from(".box" ,{
-//     x:300,
-//     opacity:0,
-//     duration:2
-
-// })
-
-
-// gsap fromto method lets you decide the both starting and ending values
-
- gsap.fromTo(".box" , {
-    opacity:0.2,
-    x:0
- } ,{
-    opacity:1,
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger)
+  
+  gsap.to(".box",{
     x:300,
-    repeat: -1,   // infinite loop
-    yoyo: true 
- })
+    rotation:360,
+    scrub:true,
+    scrollTrigger:{
+        trigger: ".section2",
+        start: "top 70%",
+        end: "+=2000",
+        scrub: 1,
+        pin: true,
+    }
+  })
 
-    document.querySelector("#play").onclick =()=> tweenTp.play()
-    document.querySelector("#pause").onclick =()=> tweenTp.pause()
-    document.querySelector("#resume").onclick =()=> tweenTp.resume()
-    document.querySelector("#restart").onclick =()=> tweenTp.restart()
-    document.querySelector("#reverse").onclick =()=> tweenTp.reverse()
-})
+    gsap.to(".image", {
+    clipPath: "inset(0 0% 0 0)",
+    scale: 1,
+    duration: 1.8,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: ".image-wrapper",
+      start: "top 15%"
+    }
+  });
+});
