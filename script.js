694c6b74360bd7ef27aeb803fe016cfd7e54f5b7
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-04-24 22:53:17 +0500

    completed

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Js connect")
+const tl = gsap.timeline();
+
+tl.from(".hero-title", {
+  y: 100,
+  opacity: 0,
+  duration: 1,
+  ease: "power3.out"
+})
+.from(".hero-text", {
+  y: 50,
+  opacity: 0,
+  duration: 0.8
+}, "-=0.5")
+.from(".hero-btn", {
+  y: 30,
+  opacity: 0,
+  duration: 0.6
+}, "-=0.4");
 });
