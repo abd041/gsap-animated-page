6a8360e251286181a3275ee2ddef5e93b69da9db
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-04-24 21:25:08 +0500

    initial commit

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(ScrollTrigger)
-  
-  gsap.to(".box",{
-    x:300,
-    rotation:360,
-    scrub:true,
-    scrollTrigger:{
-        trigger: ".section2",
-        start: "top 70%",
-        end: "+=2000",
-        scrub: 1,
-        pin: true,
-    }
-  })
-
-    gsap.to(".image", {
-    clipPath: "inset(0 0% 0 0)",
-    scale: 1,
-    duration: 1.8,
-    ease: "power4.out",
-    scrollTrigger: {
-      trigger: ".image-wrapper",
-      start: "top 15%"
-    }
-  });
+  console.log("Js connect")
 });
